a142d60d426ff0effd86d2adfb7a0ac4ebb64889
KunjPathak12 <kunjpathak1212@gmail.com>
2024-02-23 01:58:34 -0400

    chabges in Query file

--- com/DMWA_Assignment/Query.java
+++ com/DMWA_Assignment/Query.java
@@ -2,6 +2,7 @@ package com.DMWA_Assignment;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
+import java.util.regex.*;
 
 
 
@@ -9,17 +10,68 @@ public class Query {
     private ArrayList<String> queryTypes = new ArrayList<>(Arrays.asList("create", "insert", "select", "exit"));
     static Scanner scan = new Scanner(System.in);
 
-    public String create(String CreateQuery){
-        // switch (CreateQuery) {
-        //     case value:
+    public class Create{
+        String createQuery;
+        public String dbName;
+        public String table;
+        public Create(String createQuery){
+            this.createQuery = createQuery;
+        }
+    }
+
+    public class CreateDB extends Create{
+        String fileName;
+        public CreateDB(String createQuery) {
+            super(createQuery);
+            // dbName = createQuery.toLowerCase().trim().split("database")[1].trim();
+            fileName = dbName;
+        }
+        public String print(){
+            // System.out.println();
+            return "Database created";
+        }
+    }
+
+    public class CreateTable extends Create{
+        String tableName;
+        public CreateTable(String createQuery) {
+            super(createQuery);
+            // table = createQuery.toLowerCase().trim().split("table")[1].trim();
+            tableName =table;
+        }
+
+        public String print(){
+            // System.out.println();
+            return "Table Created";
+        }
+
+    }
+
+    public String parseQuery(String initQuery){
+        String str =initQuery.split(" ")[0].toLowerCase();
+        // System.out.println(str);
+        String resp="";
+        if(str.equals("create")){
+            
+            if(initQuery.contains("database")){
+                CreateDB create = new CreateDB(initQuery);
+                resp=create.print();
                 
-        //         break;
-        
-        //     default:
-        //         break;
-        // }
-        
-        return "created";
+            }
+
+            else if(initQuery.contains("table")){
+                CreateTable create = new CreateTable(initQuery);
+                resp = create.print();
+            }
+
+            
+        }
+
+        else{
+            resp = "Create not found in query";
+        }
+        // return "create Query Executed";
+        return resp;
     }
 
     public void exit(){
@@ -47,6 +99,20 @@ public class Query {
     }
 
     public static void main(String[] args) {
-
+        
+        // System.out.println(create1.dbName);
+        // System.out.println(create1.print()+" "+create2.print());
+        Pattern ptn = Pattern.compile("");
+        Matcher matcher = ptn.matcher("[int]");
+        String str1 = scan.nextLine();
+        while(!str1.contains(";")){
+            str1+=scan.nextLine();
+        }
+        // System.out.println(str1);
+        Query query = new Query();
+        System.out.println(query.parseQuery(str1));
+        // Create create = query.new Create(str1);
+        // CreateDB create1 = query.new CreateDB();
+        // CreateTable create2 = query.new CreateTable("Create database kunj1");
     }
 }
